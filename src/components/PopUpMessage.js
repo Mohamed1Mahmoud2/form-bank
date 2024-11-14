a7b3e9d4ae60d91450f61d8bc301c7dc55ff9074
Mohamed Mahmoud <103222555+Mohamed1Mahmoud2@users.noreply.github.com>
2024-11-14 19:20:13 +0200

    update

--- src/components/PopUpMessage.js
+++ src/components/PopUpMessage.js
@@ -1,13 +1,20 @@
 import './PopUpMessage.css';
 
-function PopUpMessage({ message, addClassParent, addClassCase }) {
+function PopUpMessage({ message, addClassParent, addClassCase, setCase }) {
+    function close() {
+        setCase('')
+    }
     return (
+
         <div className={addClassParent}>
+            <button className='close-btn' onClick={close}>X</button>
+
             <div className="child">
                 <p className={addClassCase}>Case:{message}</p>
             </div>
 
         </div>
+
     )
 }
 
